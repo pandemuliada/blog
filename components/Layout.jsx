d1554bf9bf7ef2ed2cc59ab06e6b745308592b9d
pandemuliada <pandemuliada@gmail.com>
2020-03-18 21:15:55 +0800

    :recycle: change og title default value

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from "./Footer"
 
 const Layout = ({ children, useContainer, ...rest }) => {
   return (<>
-    <Head title={rest.title} description={rest.description || process.env.SITE_SHORT_DESCRIPTION} {...rest} />
+    <Head title={rest.title || ""} ogTitle={rest.ogTitle || ""} description={rest.description || process.env.SITE_SHORT_DESCRIPTION} {...rest} />
     <div className={useContainer && 'w-10/12 mx-auto md:w-1/4 mt-6'}>
       {children}
       <Footer/>
