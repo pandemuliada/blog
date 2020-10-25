dfff6bfe265469388702e2d74f1651ad134776b3
pandemuliada <pandemuliada@gmail.com>
2020-10-25 12:30:03 +0800

    :sparkles: add blockquote and divider component

---
+++ components/Divider.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Divider = styled('hr')`
+  position: relative;
+  border-width: 0px;
+  width: 200px;
+
+  margin: auto;
+  margin-bottom: 60px;
+
+  text-align: center;
+
+  &::after {
+    content: '•   •   •';
+    color: ${({ theme }) =>
+      theme.isDark ? theme.colors['lighter-gray'] : theme.colors['gray']};
+    font-size: 30px;
+  }
+`
+
+export default Divider
